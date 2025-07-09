0054a01dd32e2e2150caf14f7b3eadbdb71524bc
PratibaaP <pratibaaspam@gmail.com>
2025-07-09 12:33:10 -0500

    hey there

--- ExercisingSolo/ContentView.swift
+++ ExercisingSolo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("SOlo")
+            Text("Hey therd")
         }
         .padding()
     }
