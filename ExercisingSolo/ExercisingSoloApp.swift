a800994f74108171bd32d6d0accd3e7270509145
Christina <christylin39@gmail.com>
2025-07-09 12:13:02 -0500

    Initial Commit

---
+++ ExercisingSolo/ExercisingSoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisingSoloApp.swift
+//  ExercisingSolo
+//
+//  Created by 55GOParticipant on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ExercisingSoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExercisingSoloTests/ExercisingSoloTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisingSoloTests.swift
+//  ExercisingSoloTests
+//
+//  Created by 55GOParticipant on 7/9/25.
+//
+
+import Testing
+@testable import ExercisingSolo
+
+struct ExercisingSoloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
