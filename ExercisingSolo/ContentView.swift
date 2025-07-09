4946d94e93ed889bb2bceb5549005c99741a367d
Christina <christylin39@gmail.com>
2025-07-09 12:25:16 -0500

    test 4

--- ExercisingSolo/ContentView.swift
+++ ExercisingSolo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("sunset supridse!")
+            Text(" solo leveling!")
         }
         .padding()
     }
